02a8fa78b37d0426704598783f23fae55aa13637
Karel Antonio Verdecia Ortiz <kverdecia@gmail.com>
2025-07-06 04:46:27 -0300

    issue #13: add entrypoints for dependencies installer

--- src/kvo/devtools/dependencies.py
+++ src/kvo/devtools/dependencies.py
@@ -1,6 +1,5 @@
-from typing import Protocol
-import os
 import abc
+import importlib.metadata
 import asyncio
 import subprocess
 from pathlib import Path
@@ -11,6 +10,7 @@ from pydantic import BaseModel, Field, HttpUrl
 from .types import PackageTypes
 from .errors import DependenciesError
 from .packageindexes import PythonPackageIndex
+from .package import Package
 
 
 console = rich.console.Console()
@@ -35,17 +35,6 @@ class DependenciesInstaller(BaseModel, metaclass=abc.ABCMeta):
     @property
     def package_index_url(self) -> HttpUrl | None:
         raise DependenciesError("Package index URL is not supported for this dependency installer.")
-
-    @staticmethod
-    def from_package_type(package_type: PackageTypes) -> type['DependenciesInstaller']:
-        config = {
-            PackageTypes.PYTHON_UV: PythonUvDependenciesInstaller,
-            PackageTypes.NODEJS: NodeJsDependenciesInstaller,
-        }
-        try:
-            return config[package_type]
-        except KeyError:
-            raise DependenciesError(f"There is no dependency installer registered for package type {package_type}.")
 
 
 class NodeJsDependenciesInstaller(DependenciesInstaller):
@@ -134,3 +123,32 @@ class PythonUvDependenciesInstaller(DependenciesInstaller):
                 f"Error installing Python dependencies in {self.package_dir}: {stderr.decode('utf-8') if stderr else ''}"
             )
         console.log(f"Python dependencies installed successfully in {self.package_dir}.", style="bold green")
+
+
+async def install_dependencies(package: Package, package_index: str | None = None) -> None:
+    """
+    Installs dependencies for a given package synchronously.
+    This function is a wrapper around the package type dependency installer class.
+    
+    For each package type you want to support, there must be an entry point registered
+    in the group `kvo-devtools-dependencies-installer` with the same name of the package
+    type.
+    
+    :param package: The package for which to install dependencies.
+    :param package_index: Optional package index URL to use for installation.
+    """
+    if package.type is None:
+        raise ValueError("Package type is not set for this package.")
+    entrypoints_group_name = 'kvo-devtools-dependencies-installer'
+    entrypoints = importlib.metadata.entry_points(group=entrypoints_group_name)
+    if not entrypoints:
+        raise DependenciesError(f"No entry points found for group '{entrypoints_group_name}'. Ensure you have registered the dependency installer for package type '{package.type}'.")
+    entrypoint = next((ep for ep in entrypoints if ep.name == package.type.value), None)
+    if entrypoint is None:
+        raise DependenciesError(f"No entry point found for package type '{package.type}' in group '{entrypoints_group_name}'. Ensure you have registered the dependency installer for this package type.")
+    Installer = entrypoint.load()
+    if not issubclass(Installer, DependenciesInstaller):
+        raise DependenciesError(f"The entry point '{entrypoint.name}' does not point to a valid DependenciesInstaller subclass.")
+    console.log(f"Installing dependencies for package '{package.name}' of type '{package.type}' using entry point value '{entrypoint.value}'.")
+    installer = Installer(package_dir=package.path, package_index=package_index or package.package_index)
+    await installer.install()
